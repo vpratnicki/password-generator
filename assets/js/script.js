e47c57c80c8af413d7785ee9c736a9cc8f9a485d
vpratnicki <vanessa@pratnicki.net>
2022-08-18 18:26:52 -0400

    set up specfic functions to randomly generate each value. Added form elements to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,108 @@
 // Assignment code here
+/* Password requirements
+ - length needs to be between 8-128 charatcer
+ - choose uppercase letters
+ - choose lowercase letter
+ - choose numbers
+ - choose special characters */
+
+ //DOM elements
+ const passwordEl = document.getElementById('password');
+ const lengthEl = document.getElementById('length');
+ const upperCaseEl = document.getElementById('uppercase');
+ const lowerCaseEl = document.getElementById('lowercase');
+ const numbersEl = document.getElementById('numbers');
+ const specialsEl = document.getElementById('specials');
+ const generateEl = document.getElementById('generate');
 
 
-// Password requirements
-// length needs to be between 8-128 charatcer
+//specific for each requirement  (www.net-comber.com/charset.html)
+//generate lowercase letter
+ function getRandomLower () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+ }
 
-// defining variables 
-// var capLeters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// var lowerLetter = 'abcdefghijklmnopqrstuvwxyz';
-// var numbers = '123456789';
-// var specialChars = '!@#$%^&*()';
+ //generate uppercase leter
+ function getRandomUpper () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+ }
+ 
+ //geneter random number
+ function getRandomNumber () {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+ }
+
+  //generate random symbol
+ function getRandomSpecials () {
+  const specials = '!()@#$%^&*+=<>,.?';
+  return specials[Math.floor(Math.random() * specials.length)];
+ }
 
 
-//function to determine length
-var getPasswordLength = function () {
-  // Ask user for the number of characters they would like to use
-  var length = window.prompt("Select the number of characters you would like for your password. Select a character length between 8 and 128");
-  // convert answer to number
-  length = parseInt(length);
+// //function to determine length
+// var getPasswordLength = function () {
+//   // Ask user for the number of characters they would like to use
+//   var length = window.prompt("Select the number of characters you would like for your password. Select a character length between 8 and 128");
+//   // convert answer to number
+//   length = parseInt(length);
 
-  // if number is outside of range, inform them it needs to be within range
-  if (length < 8 || length > 128) {
-    window.alert("Your password needs to be between 8 and 128 characters long");
-    return getPasswordLength();
-  }
+//   // if number is outside of range, inform them it needs to be within range
+//   if (length < 8 || length > 128) {
+//     window.alert("Your password needs to be between 8 and 128 characters long");
+//     return getPasswordLength();
+//   }
 
-  else {
-    window.alert("You entered " + length);
-    console.log(length);
-    getCharacters();
-  }
-};
+//   else {
+//     window.alert("You entered " + length);
+//     console.log(length);
+//     getCharacters();
+//   }
+// };
 
 //function to determine if user wants letters (if any)
-var getCharacters = function () {
+// var getCharacters = function () {
   // Ask which type of characters they would like
-  var CharOption = window.prompt("Character types. Select one of the four optons. 1: Uppercase letters only. 2: Lowercase letter only. 3: Mix of lower and uppercase letters. 4: Do not used letters. ");
+  // var CharOption = window.prompt("Character types. Select one of the four optons. 1: Uppercase letters only. 2: Lowercase letter only. 3: Mix of lower and uppercase letters. 4: Do not used letters. ");
 
   // convert answer to number
-  CharOption = parseInt(CharOption);
+//   CharOption = parseInt(CharOption);
 
-  switch (CharOption) {
-    case 1:
-      //upperOnly();
-      console.log("option 1");
-      break;
-    case 2:
-      //lowerOnly();
-      console.log("option 2");
-      break;
-    case 3:
-      //charMix();
-      console.log("option 3");
-      break;
-    case 4:
-      //noLetters();
-      console.log("option 4");
-      break;
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-      // restart function again get user to pick a valid option
-      getCharacters();
-      break;
-  }
+//   switch (CharOption) {
+//     case 1:
+//       //upperOnly();
+//       console.log("option 1");
+//       break;
+//     case 2:
+//       //lowerOnly();
+//       console.log("option 2");
+//       break;
+//     case 3:
+//       //charMix();
+//       console.log("option 3");
+//       break;
+//     case 4:
+//       //noLetters();
+//       console.log("option 4");
+//       break;
+//     default:
+//       window.alert("You did not pick a valid option. Try again.");
+//       // restart function again get user to pick a valid option
+//       getCharacters();
+//       break;
+//   }
   
-};
+// };
 
 //function to determine if users wants numbers
 
 
 
-var passwordReq = {
-  length: getPasswordLength(),
+
+// var passwordReq = {
+//   length: getPasswordLength(),
   // upperOnly: function {
   // 
-}
+// }
 
 
   // CharOption: getCharacters() 
@@ -88,16 +117,16 @@ var passwordReq = {
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
